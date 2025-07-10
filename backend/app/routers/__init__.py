d0fdb12ebb4d3dc190e1d2a73e544b6b1b6d24a4
tahsinchoudhury <tahsinbd247@gmail.com>
2025-07-11 02:33:50 +0600

    merged

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter
-<<<<<<< HEAD
-from routers import token, auth, img, profile, homepage, faculty, course, notice, classroom, committee, committee_member, research_assistant, equipment, equipment_booking
-=======
-from routers import token, auth, img, profile, homepage, faculty, notice, classroom, committee, committee_member, research_assistant, office_room, faculty
->>>>>>> fc486b2d5daa44effe5d5eac9e64b9e3610f2359
 
+from routers import token, auth, img, profile, homepage, faculty, notice, classroom, committee, committee_member, research_assistant, equipment, equipment_booking, office_room
 router = APIRouter(
     prefix="/api/v1"
 )
@@ -20,10 +16,6 @@ router.include_router(classroom.router)
 router.include_router(committee.router)
 router.include_router(committee_member.router)
 router.include_router(research_assistant.router)
-<<<<<<< HEAD
 router.include_router(equipment.router)
 router.include_router(equipment_booking.router)
-=======
 router.include_router(office_room.router)
-router.include_router(faculty.router)
->>>>>>> fc486b2d5daa44effe5d5eac9e64b9e3610f2359
